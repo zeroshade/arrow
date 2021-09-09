d7a1e9769ff3e124f93701fc18caa5a7a7e7149f
Matthew Topol <mtopol@factset.com>
2021-09-09 14:17:13 +0000

    concatenate func and bitmap reader/writers

--- go/arrow/bitutil/bitutil.go
+++ go/arrow/bitutil/bitutil.go
@@ -17,9 +17,14 @@
 package bitutil
 
 import (
+	"math"
 	"math/bits"
 	"reflect"
 	"unsafe"
+
+	"github.com/apache/arrow/go/arrow/endian"
+	"github.com/apache/arrow/go/arrow/internal/debug"
+	"github.com/apache/arrow/go/arrow/memory"
 )
 
 var (
@@ -156,4 +161,418 @@ func bytesToUint64(b []byte) []uint64 {
 	s.Cap = h.Cap / uint64SizeBytes
 
 	return res
+}
+
+var (
+	// PrecedingBitmask is a convenience set of values as bitmasks for checking
+	// prefix bits of a byte
+	PrecedingBitmask = [8]byte{0, 1, 3, 7, 15, 31, 63, 127}
+	// TrailingBitmask is the bitwise complement version of kPrecedingBitmask
+	TrailingBitmask = [8]byte{255, 254, 252, 248, 240, 224, 192, 128}
+)
+
+// SetBitsTo is a convenience function to quickly set or unset all the bits
+// in a bitmap starting at startOffset for length bits.
+func SetBitsTo(bits []byte, startOffset, length int64, areSet bool) {
+	if length == 0 {
+		return
+	}
+
+	beg := startOffset
+	end := startOffset + length
+	var fill uint8 = 0
+	if areSet {
+		fill = math.MaxUint8
+	}
+
+	byteBeg := beg / 8
+	byteEnd := end/8 + 1
+
+	// don't modify bits before the startOffset by using this mask
+	firstByteMask := PrecedingBitmask[beg%8]
+	// don't modify bits past the length by using this mask
+	lastByteMask := TrailingBitmask[end%8]
+
+	if byteEnd == byteBeg+1 {
+		// set bits within a single byte
+		onlyByteMask := firstByteMask
+		if end%8 != 0 {
+			onlyByteMask = firstByteMask | lastByteMask
+		}
+
+		bits[byteBeg] &= onlyByteMask
+		bits[byteBeg] |= fill &^ onlyByteMask
+		return
+	}
+
+	// set/clear trailing bits of first byte
+	bits[byteBeg] &= firstByteMask
+	bits[byteBeg] |= fill &^ firstByteMask
+
+	if byteEnd-byteBeg > 2 {
+		memory.Set(bits[byteBeg+1:byteEnd-1], fill)
+	}
+
+	if end%8 == 0 {
+		return
+	}
+
+	bits[byteEnd-1] &= lastByteMask
+	bits[byteEnd-1] |= fill &^ lastByteMask
+}
+
+var toFromLEFunc func(uint64) uint64
+
+func init() {
+	if endian.IsBigEndian {
+		toFromLEFunc = bits.ReverseBytes64
+	} else {
+		toFromLEFunc = func(in uint64) uint64 { return in }
+	}
+}
+
+type BitmapWriter struct {
+	buf    []byte
+	pos    int64
+	length int64
+
+	curByte    uint8
+	bitMask    uint8
+	byteOffset int64
+}
+
+// NewBitmapWriter returns a sequential bitwise writer that preserves surrounding
+// bit values as it writes.
+func NewBitmapWriter(bitmap []byte, start, length int64) *BitmapWriter {
+	ret := &BitmapWriter{
+		buf:        bitmap,
+		length:     length,
+		byteOffset: start / 8,
+		bitMask:    BitMask[start%8],
+	}
+	if length > 0 {
+		ret.curByte = bitmap[int(ret.byteOffset)]
+	}
+	return ret
+}
+
+func (b *BitmapWriter) Reset(start, length int64) {
+	b.pos = 0
+	b.byteOffset = start / 8
+	b.bitMask = BitMask[start%8]
+	b.length = length
+	if b.length > 0 {
+		b.curByte = b.buf[int(b.byteOffset)]
+	}
+}
+
+func (b *BitmapWriter) Pos() int64 { return b.pos }
+func (b *BitmapWriter) Set()       { b.curByte |= b.bitMask }
+func (b *BitmapWriter) Clear()     { b.curByte &= ^b.bitMask }
+
+func (b *BitmapWriter) Next() {
+	b.bitMask = b.bitMask << 1
+	b.pos++
+	if b.bitMask == 0 {
+		b.bitMask = 0x01
+		b.buf[b.byteOffset] = b.curByte
+		b.byteOffset++
+		if b.pos < b.length {
+			b.curByte = b.buf[int(b.byteOffset)]
+		}
+	}
+}
+
+func (b *BitmapWriter) Finish() {
+	if b.length > 0 && (b.bitMask != 0x01 || b.pos < b.length) {
+		b.buf[int(b.byteOffset)] = b.curByte
+	}
+}
+
+type BitmapWordWriter struct {
+	bitmap []byte
+	offset int64
+	len    int64
+
+	bitMask     uint64
+	currentWord uint64
+}
+
+func NewBitmapWordWriter(bitmap []byte, start int64, len int64) *BitmapWordWriter {
+	ret := &BitmapWordWriter{
+		bitmap:  bitmap[start/8:],
+		len:     len,
+		offset:  start % 8,
+		bitMask: (uint64(1) << uint64(start%8)) - 1,
+	}
+
+	if ret.offset != 0 {
+		if ret.len >= int64(unsafe.Sizeof(uint64(0))*8) {
+			ret.currentWord = toFromLEFunc(endian.Native.Uint64(ret.bitmap))
+		} else if ret.len > 0 {
+			ret.currentWord = toFromLEFunc(uint64(ret.bitmap[0]))
+		}
+	}
+	return ret
+}
+
+func (bm *BitmapWordWriter) PutNextWord(word uint64) {
+	sz := int(unsafe.Sizeof(word))
+	if bm.offset != 0 {
+		// split one word into two adjacent words, don't touch unused bits
+		//               |<------ word ----->|
+		//               +-----+-------------+
+		//               |  A  |      B      |
+		//               +-----+-------------+
+		//                  |         |
+		//                  v         v       offset
+		// +-------------+-----+-------------+-----+
+		// |     ---     |  A  |      B      | --- |
+		// +-------------+-----+-------------+-----+
+		// |<------ next ----->|<---- current ---->|
+		// fmt.Printf("%# b\n", word)
+		word = (word << uint64(bm.offset)) | (word >> (int64(sz*8) - bm.offset))
+		// fmt.Printf("%-#b\n", word)
+		next := endian.Native.Uint64(bm.bitmap[sz:])
+		// fmt.Printf("%#0b\n", bm.currentWord)
+		bm.currentWord = (bm.currentWord & bm.bitMask) | (word &^ bm.bitMask)
+		// fmt.Printf("%0#b\n", bm.currentWord)
+		next = (next &^ bm.bitMask) | (word & bm.bitMask)
+		// fmt.Printf("%#0b\n", next)
+		endian.Native.PutUint64(bm.bitmap, toFromLEFunc(bm.currentWord))
+		endian.Native.PutUint64(bm.bitmap[sz:], toFromLEFunc(next))
+		bm.currentWord = next
+	} else {
+		endian.Native.PutUint64(bm.bitmap, toFromLEFunc(word))
+	}
+	bm.bitmap = bm.bitmap[sz:]
+}
+
+func (bm *BitmapWordWriter) PutNextTrailingByte(b byte, validBits int) {
+	curbyte := (*[8]byte)(unsafe.Pointer(&bm.currentWord))[0]
+	if validBits == 8 {
+		if bm.offset != 0 {
+			b = (b << bm.offset) | (b >> (8 - bm.offset))
+			next := bm.bitmap[1]
+			curbyte = (curbyte & byte(bm.bitMask)) | (b &^ byte(bm.bitMask))
+			next = (next &^ byte(bm.bitMask)) | (b & byte(bm.bitMask))
+			bm.bitmap[0] = curbyte
+			bm.bitmap[1] = next
+			bm.currentWord = uint64(next)
+		} else {
+			bm.bitmap[0] = b
+		}
+		bm.bitmap = bm.bitmap[1:]
+	} else {
+		debug.Assert(validBits > 0 && validBits < 8, "invalid valid bits in bitmap word writer")
+		debug.Assert(BytesForBits(bm.offset+int64(validBits)) <= int64(len(bm.bitmap)), "writing trailiing byte outside of bounds of bitmap")
+		wr := NewBitmapWriter(bm.bitmap, bm.offset, int64(validBits))
+		for i := 0; i < validBits; i++ {
+			if b&0x01 != 0 {
+				wr.Set()
+			} else {
+				wr.Clear()
+			}
+			wr.Next()
+			b >>= 1
+		}
+		wr.Finish()
+	}
+}
+
+// BitmapReader is a simple bitmap reader for a byte slice.
+type BitmapReader struct {
+	bitmap []byte
+	pos    int64
+	len    int64
+
+	current    byte
+	byteOffset int64
+	bitOffset  int64
+}
+
+// NewBitmapReader creates and returns a new bitmap reader for the given bitmap
+func NewBitmapReader(bitmap []byte, offset, length int64) *BitmapReader {
+	curbyte := byte(0)
+	if length > 0 && bitmap != nil {
+		curbyte = bitmap[offset/8]
+	}
+	return &BitmapReader{
+		bitmap:     bitmap,
+		byteOffset: offset / 8,
+		bitOffset:  offset % 8,
+		current:    curbyte,
+		len:        length,
+	}
+}
+
+// Set returns true if the current bit is set
+func (b *BitmapReader) Set() bool {
+	return (b.current & (1 << b.bitOffset)) != 0
+}
+
+// NotSet returns true if the current bit is not set
+func (b *BitmapReader) NotSet() bool {
+	return (b.current & (1 << b.bitOffset)) == 0
+}
+
+// Next advances the reader to the next bit in the bitmap.
+func (b *BitmapReader) Next() {
+	b.bitOffset++
+	b.pos++
+	if b.bitOffset == 8 {
+		b.bitOffset = 0
+		b.byteOffset++
+		if b.pos < b.len {
+			b.current = b.bitmap[int(b.byteOffset)]
+		}
+	}
+}
+
+// Pos returns the current bit position in the bitmap that the reader is looking at
+func (b *BitmapReader) Pos() int64 { return b.pos }
+
+// Len returns the total number of bits in the bitmap
+func (b *BitmapReader) Len() int64 { return b.len }
+
+type BitmapWordReader struct {
+	bitmap        []byte
+	offset        int64
+	nwords        int64
+	trailingBits  int
+	trailingBytes int
+	curword       uint64
+}
+
+func NewBitmapWordReader(bitmap []byte, offset, length int64) *BitmapWordReader {
+	bitoffset := offset % 8
+	byteOffset := offset / 8
+	bm := &BitmapWordReader{
+		offset: bitoffset,
+		bitmap: bitmap[byteOffset : byteOffset+BytesForBits(bitoffset+length)],
+		// decrement wordcount by 1 as we may touch two adjacent words in one iteration
+		nwords: length/int64(unsafe.Sizeof(uint64(0))*8) - 1,
+	}
+	if bm.nwords < 0 {
+		bm.nwords = 0
+	}
+	bm.trailingBits = int(length - bm.nwords*int64(unsafe.Sizeof(uint64(0)))*8)
+	bm.trailingBytes = int(BytesForBits(int64(bm.trailingBits)))
+
+	if bm.nwords > 0 {
+		bm.curword = toFromLEFunc(endian.Native.Uint64(bm.bitmap))
+	} else {
+		bm.curword = toFromLEFunc(uint64(bm.bitmap[0]))
+	}
+	return bm
+}
+
+func (bm *BitmapWordReader) NextWord() uint64 {
+	bm.bitmap = bm.bitmap[unsafe.Sizeof(bm.curword):]
+	word := bm.curword
+	nextWord := toFromLEFunc(endian.Native.Uint64(bm.bitmap))
+	if bm.offset != 0 {
+		// combine two adjacent words into one word
+		// |<------ next ----->|<---- current ---->|
+		// +-------------+-----+-------------+-----+
+		// |     ---     |  A  |      B      | --- |
+		// +-------------+-----+-------------+-----+
+		//                  |         |       offset
+		//                  v         v
+		//               +-----+-------------+
+		//               |  A  |      B      |
+		//               +-----+-------------+
+		//               |<------ word ----->|
+		word >>= uint64(bm.offset)
+		word |= nextWord << (int64(unsafe.Sizeof(uint64(0))*8) - bm.offset)
+	}
+	bm.curword = nextWord
+	return word
+}
+
+func (bm *BitmapWordReader) NextTrailingByte() (val byte, validBits int) {
+	debug.Assert(bm.trailingBits > 0, "next trailing byte called with no trailing bits")
+
+	if bm.trailingBits <= 8 {
+		// last byte
+		validBits = bm.trailingBits
+		bm.trailingBits = 0
+		rdr := NewBitmapReader(bm.bitmap, bm.offset, int64(validBits))
+		for i := 0; i < validBits; i++ {
+			val >>= 1
+			if rdr.Set() {
+				val |= 0x80
+			}
+			rdr.Next()
+		}
+		val >>= (8 - validBits)
+		return
+	}
+
+	// fmt.Printf("%#b\n", bm.curword)
+	bm.bitmap = bm.bitmap[1:]
+	nextByte := bm.bitmap[0]
+	val = (*[8]byte)(unsafe.Pointer(&bm.curword))[0]
+	if bm.offset != 0 {
+		val >>= byte(bm.offset)
+		val |= nextByte << (8 - bm.offset)
+	}
+	(*[8]byte)(unsafe.Pointer(&bm.curword))[0] = nextByte
+	bm.trailingBits -= 8
+	bm.trailingBytes--
+	validBits = 8
+	return
+}
+
+func (bm *BitmapWordReader) Words() int64       { return bm.nwords }
+func (bm *BitmapWordReader) TrailingBytes() int { return bm.trailingBytes }
+
+func CopyBitmap(src []byte, srcOffset, length int64, dst []byte, dstOffset int64) {
+	if length == 0 {
+		return
+	}
+
+	bitOffset := srcOffset % 8
+	destBitOffset := dstOffset % 8
+
+	if bitOffset != 0 || destBitOffset != 0 {
+		rdr := NewBitmapWordReader(src, srcOffset, length)
+		wr := NewBitmapWordWriter(dst, dstOffset, length)
+
+		nwords := rdr.Words()
+		for nwords > 0 {
+			nwords--
+			wr.PutNextWord(rdr.NextWord())
+		}
+		nbytes := rdr.TrailingBytes()
+		for nbytes > 0 {
+			nbytes--
+			bt, validBits := rdr.NextTrailingByte()
+			wr.PutNextTrailingByte(bt, validBits)
+		}
+		return
+	}
+
+	if length == 0 {
+		return
+	}
+
+	nbytes := BytesForBits(length)
+
+	// shift by its byte offset
+	src = src[srcOffset/8:]
+	dst = dst[dstOffset/8:]
+
+	// Take care of the trailing bits in the last byte
+	// E.g., if trailing_bits = 5, last byte should be
+	// - low  3 bits: new bits from last byte of data buffer
+	// - high 5 bits: old bits from last byte of dest buffer
+	trailingBits := nbytes*8 - length
+	trailMask := byte(uint(1)<<(8-trailingBits)) - 1
+
+	copy(dst, src[:nbytes-1])
+	lastData := src[nbytes-1]
+
+	dst[nbytes-1] &= ^trailMask
+	dst[nbytes-1] |= lastData & trailMask
 }
--- go/arrow/bitutil/bitutil_test.go
+++ go/arrow/bitutil/bitutil_test.go
@@ -19,6 +19,7 @@ package bitutil_test
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"testing"
 
 	"github.com/apache/arrow/go/arrow/bitutil"
@@ -126,6 +127,39 @@ func TestSetBitTo(t *testing.T) {
 	assert.Equal(t, []byte{0xa1, 0xc2}, buf)
 }
 
+func TestSetBitsTo(t *testing.T) {
+	for _, fillByte := range []byte{0x00, 0xFF} {
+		{
+			// set within a byte
+			bm := []byte{fillByte, fillByte, fillByte, fillByte}
+			bitutil.SetBitsTo(bm, 2, 2, true)
+			bitutil.SetBitsTo(bm, 4, 2, false)
+			assert.Equal(t, []byte{(fillByte &^ 0x3C) | 0xC}, bm[:1])
+		}
+		{
+			// test straddling a single byte boundary
+			bm := []byte{fillByte, fillByte, fillByte, fillByte}
+			bitutil.SetBitsTo(bm, 4, 7, true)
+			bitutil.SetBitsTo(bm, 11, 7, false)
+			assert.Equal(t, []byte{(fillByte & 0xF) | 0xF0, 0x7, fillByte &^ 0x3}, bm[:3])
+		}
+		{
+			// test byte aligned end
+			bm := []byte{fillByte, fillByte, fillByte, fillByte}
+			bitutil.SetBitsTo(bm, 4, 4, true)
+			bitutil.SetBitsTo(bm, 8, 8, false)
+			assert.Equal(t, []byte{(fillByte & 0xF) | 0xF0, 0x00, fillByte}, bm[:3])
+		}
+		{
+			// test byte aligned end, multiple bytes
+			bm := []byte{fillByte, fillByte, fillByte, fillByte}
+			bitutil.SetBitsTo(bm, 0, 24, false)
+			falseByte := byte(0)
+			assert.Equal(t, []byte{falseByte, falseByte, falseByte, fillByte}, bm)
+		}
+	}
+}
+
 func TestCountSetBits(t *testing.T) {
 	tests := []struct {
 		name string
@@ -193,6 +227,114 @@ func TestCountSetBitsOffset(t *testing.T) {
 		got := bitutil.CountSetBits(buf, offset, nbits-offset)
 		if got != want {
 			t.Errorf("offset[%2d/%2d]=%5d. got=%5d, want=%5d", i+1, len(offsets), offset, got, want)
+		}
+	}
+}
+
+func writeToWriter(vals []int, wr *bitutil.BitmapWriter) {
+	for _, v := range vals {
+		if v != 0 {
+			wr.Set()
+		} else {
+			wr.Clear()
+		}
+		wr.Next()
+	}
+	wr.Finish()
+}
+
+func bitmapFromSlice(vals []int, offset int64) []byte {
+	out := make([]byte, bitutil.BytesForBits(int64(len(vals))+offset))
+	wr := bitutil.NewBitmapWriter(out, offset, int64(len(vals)))
+	writeToWriter(vals, wr)
+	return out
+}
+
+func TestBitmapWriter(t *testing.T) {
+	for _, fillByte := range []byte{0x00, 0xFF} {
+		{
+			bitmap := []byte{fillByte, fillByte, fillByte, fillByte}
+			wr := bitutil.NewBitmapWriter(bitmap, 0, 12)
+			writeToWriter([]int{0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1}, wr)
+			// {0b00110110, 0b....1010, ........, ........}
+			assert.Equal(t, []byte{0x36, (0x0A | (fillByte & 0xF0)), fillByte, fillByte}, bitmap)
+		}
+		{
+			bitmap := []byte{fillByte, fillByte, fillByte, fillByte}
+			wr := bitutil.NewBitmapWriter(bitmap, 3, 12)
+			writeToWriter([]int{0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1}, wr)
+			// {0b10110..., 0b.1010001, ........, ........}
+			assert.Equal(t, []byte{0xb0 | (fillByte & 0x07), 0x51 | (fillByte & 0x80), fillByte, fillByte}, bitmap)
+		}
+		{
+			bitmap := []byte{fillByte, fillByte, fillByte, fillByte}
+			wr := bitutil.NewBitmapWriter(bitmap, 20, 12)
+			writeToWriter([]int{0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1}, wr)
+			// {........, ........, 0b0110...., 0b10100011}
+			assert.Equal(t, []byte{fillByte, fillByte, 0x60 | (fillByte & 0x0f), 0xa3}, bitmap)
+		}
+	}
+}
+
+func TestBitmapReader(t *testing.T) {
+	assertReaderVals := func(vals []int, rdr *bitutil.BitmapReader) {
+		for _, v := range vals {
+			if v != 0 {
+				assert.True(t, rdr.Set())
+				assert.False(t, rdr.NotSet())
+			} else {
+				assert.False(t, rdr.Set())
+				assert.True(t, rdr.NotSet())
+			}
+			rdr.Next()
+		}
+	}
+
+	for _, offset := range []int64{0, 1, 3, 5, 7, 8, 12, 13, 21, 38, 75, 120} {
+		bm := bitmapFromSlice([]int{0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1}, offset)
+
+		rdr := bitutil.NewBitmapReader(bm, offset, 14)
+		assertReaderVals([]int{0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1}, rdr)
+	}
+}
+
+func TestCopyBitmap(t *testing.T) {
+	const bufsize = 18
+	lengths := []int64{bufsize*8 - 4, bufsize * 8}
+	offsets := []int64{0, 12, 16, 32, 37, 63, 64, 128}
+
+	buffer := make([]byte, bufsize)
+
+	// random bytes
+	r := rand.New(rand.NewSource(0))
+	r.Read(buffer)
+
+	// add 16 byte padding
+	otherBuffer := make([]byte, bufsize+32)
+	r.Read(otherBuffer)
+
+	for _, nbits := range lengths {
+		for _, offset := range offsets {
+			for _, destOffset := range offsets {
+				t.Run(fmt.Sprintf("bits %d off %d dst %d", nbits, offset, destOffset), func(t *testing.T) {
+					copyLen := nbits - offset
+
+					bmCopy := make([]byte, len(otherBuffer))
+					copy(bmCopy, otherBuffer)
+
+					bitutil.CopyBitmap(buffer, offset, copyLen, bmCopy, destOffset)
+
+					for i := 0; i < int(destOffset); i++ {
+						assert.Equalf(t, bitutil.BitIsSet(otherBuffer, i), bitutil.BitIsSet(bmCopy, i), "bit index: %d", i)
+					}
+					for i := 0; i < int(copyLen); i++ {
+						assert.Equalf(t, bitutil.BitIsSet(buffer, i+int(offset)), bitutil.BitIsSet(bmCopy, i+int(destOffset)), "bit index: %d", i)
+					}
+					for i := int(destOffset + copyLen); i < len(otherBuffer); i++ {
+						assert.Equalf(t, bitutil.BitIsSet(otherBuffer, i), bitutil.BitIsSet(bmCopy, i), "bit index: %d", i)
+					}
+				})
+			}
 		}
 	}
 }
@@ -284,4 +426,49 @@ func BenchmarkCountSetBitsOffset_1000(b *testing.B) {
 
 func BenchmarkCountSetBitsOffset_1024(b *testing.B) {
 	benchmarkCountSetBitsN(b, 1, 1024)
+}
+
+func benchmarkCopyBitmapN(b *testing.B, offsetSrc, offsetDest int64, n int) {
+	nbits := int64(n * 8)
+	// random bytes
+	r := rand.New(rand.NewSource(0))
+	src := make([]byte, n)
+	r.Read(src)
+
+	length := nbits - offsetSrc
+
+	dest := make([]byte, bitutil.BytesForBits(length+offsetDest))
+
+	b.ResetTimer()
+	b.SetBytes(int64(n))
+	for i := 0; i < b.N; i++ {
+		bitutil.CopyBitmap(src, offsetSrc, length, dest, offsetDest)
+	}
+}
+
+// Fast path which is just a memcopy
+func BenchmarkCopyBitmapWithoutOffset(b *testing.B) {
+	for _, sz := range []int{32, 128, 1000, 1024} {
+		b.Run(strconv.Itoa(sz), func(b *testing.B) {
+			benchmarkCopyBitmapN(b, 0, 0, sz)
+		})
+	}
+}
+
+// slow path where the source buffer is not byte aligned
+func BenchmarkCopyBitmapWithOffset(b *testing.B) {
+	for _, sz := range []int{32, 128, 1000, 1024} {
+		b.Run(strconv.Itoa(sz), func(b *testing.B) {
+			benchmarkCopyBitmapN(b, 4, 0, sz)
+		})
+	}
+}
+
+// slow path where both source and dest are not byte aligned
+func BenchmarkCopyBitmapWithOffsetBoth(b *testing.B) {
+	for _, sz := range []int{32, 128, 1000, 1024} {
+		b.Run(strconv.Itoa(sz), func(b *testing.B) {
+			benchmarkCopyBitmapN(b, 3, 7, sz)
+		})
+	}
 }
